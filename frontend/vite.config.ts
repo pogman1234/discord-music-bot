8dc6c355f229b560e7097b589f5248c0a2954366
Nicholas Carson <38814189+pogman1234@users.noreply.github.com>
2025-01-15 17:25:57 -0500

    Updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Specify the output directory as 'build'
+  },
+  resolve: {
+    alias: {
+      'readable-stream': 'vite-compatible-readable-stream'
+    }
+  }
+});
